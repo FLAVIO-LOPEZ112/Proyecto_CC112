5cd52c01bdafc1d21055038742cd54d1e0a6d596
BrianLaTorre <brianjairlatorre@gmail.com>
2024-12-01 15:10:13 -0500

    CrearBoard

--- Parte2/buscaminas.py
+++ Parte2/buscaminas.py
@@ -1,9 +1,34 @@
+import random
+
 class Board:
     def __init__(self, n, num_bombas):
         self.n=n
         self.num_bombas=num_bombas
 
-        self.board = self.crear_board()
+        self.board=self.crear_board()
+        self.colocar_valores()
+        self.dug=set()
+
+    def crear_board(self):
+
+        board=[[None for _ in range (self.dim_size)] for _ in range(self.n)]
+
+        bombas_colocadas=0
+        while bombas_colocadas < self.num_bombas:
+            ubicacion = random. randint(0, self.dim_size**2 - 1)
+            fila=ubicacion//self.n
+            columna=ubicacion% self.n
+
+            if board[fila][columna]=='*':
+                continue
+
+            board[fila][columna]= '*'
+            bombas_colocadas+=1
+
+        return board
+
+
+
 
 
     # Separación -----
