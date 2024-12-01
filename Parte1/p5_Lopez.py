c9ca8382777ec77dfcf29c98747923f13d6f00a0
FLAVIO-LOPEZ112 <flavio.lopez.o@uni.pe>
2024-12-01 11:09:54 -0500

    modificacion del problema 5

--- Parte1/p5_Lopez.py
+++ Parte1/p5_Lopez.py
@@ -1,11 +1,13 @@
 
 #Archivos en Python
 #2. Escribir datos en un archivo de texto
-"""
-with open("D:Parte1\datos1.txt", "w") as archivo:
+
+with open("Parte1\datos1.txt", "w") as archivo:
     archivo.write("Lopez\nOcampo\nFlavio\nRaul")
+
 """
 archivo = open("D:Parte1\datos1.txt", "w")
 archivo.write("Lopez\nOcampo\nFlavio\nRaul")
 print("se agregaron los datos correctamente")
 archivo.close()
+"""
