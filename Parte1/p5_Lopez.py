956d26844b50be5e6e0c365acb8dbfe5c5ece0db
BrianLaTorre <brianjairlatorre@gmail.com>
2024-12-01 11:21:32 -0500

    cambio p5

--- Parte1/p5_Lopez.py
+++ Parte1/p5_Lopez.py
@@ -6,7 +6,7 @@ with open("Parte1\datos1.txt", "w") as archivo:
     archivo.write("Lopez\nOcampo\nFlavio\nRaul")
 
 """
-archivo = open("D:Parte1\datos1.txt", "w")
+archivo = open("Parte1\datos1.txt", "w")
 archivo.write("Lopez\nOcampo\nFlavio\nRaul")
 print("se agregaron los datos correctamente")
 archivo.close()
