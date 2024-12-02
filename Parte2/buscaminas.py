b1a65292d51d712a76c1f8445c34b8bc14faa47b
BrianLaTorre <brianjairlatorre@gmail.com>
2024-12-01 19:41:42 -0500

    90% terminado parte 2

--- Parte2/buscaminas.py
+++ Parte2/buscaminas.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 class Board:
     def __init__(self, n, num_bombas):
@@ -6,16 +7,16 @@ class Board:
         self.num_bombas=num_bombas
 
         self.board=self.crear_board()
-        self.colocar_valores()
+        self.asignar_valores_a_la_matriz() #revisar aqui
         self.cavado=set()
 
     def crear_board(self):
 
-        board=[[None for _ in range (self.dim_size)] for _ in range(self.n)]
+        board=[[None for _ in range (self.n)] for _ in range(self.n)]
 
         bombas_colocadas=0
         while bombas_colocadas < self.num_bombas:
-            ubicacion = random. randint(0, self.dim_size**2 - 1)
+            ubicacion = random. randint(0, self.n**2 - 1)
             fila=ubicacion//self.n
             columna=ubicacion% self.n
 
@@ -26,8 +27,6 @@ class Board:
             bombas_colocadas+=1
 
         return board
-    
-    # Separación -----
 
     def asignar_valores_a_la_matriz(self):
         for f in range(self.n):
@@ -43,7 +42,7 @@ class Board:
             for c in range(max(0,columna-1),min(self.n,(columna+1)+1)):
                 if f == fila and c== columna:
                     continue
-                elif self.boar[f][c] == "*":
+                elif self.board[f][c] == "*":
                     numero_de_bombas_cercanas += 1
 
         return numero_de_bombas_cercanas
@@ -69,5 +68,32 @@ class Board:
                 else:
                     tablero_visible[f][c] = " "
         
-def play(n=10, num_bombas=10):
-    pass
+def play(n, num_bombas):
+    board= Board(n, num_bombas)
+
+    safe= True
+    while len(board.cavado) < board.n **2 - num_bombas:
+        print(board)
+        entrada_usuario=re.split(',(\\s)*',input("Dónde te gustaría cavar? (fila,columna): "))
+        fila, columna= int(entrada_usuario[0]), int(entrada_usuario[-1])
+        if fila < 0 or fila >= board.n or col <0 or col>= n:
+            print("Ingrese una posición válida. ")
+            continue
+
+        safe= board.cavar(fila,columna)
+        if not safe: 
+            break
+
+    if safe: 
+        print("¡Felicitaciones! Has ganado.")
+    else:
+        print("Perdiste, vuelve a intentarlo.")
+        board.cavar=[(fila, columna) for fila in range (board.n) for columna in range(board.n)]
+        print(board)
+
+n=10
+num_bombas=10
+
+play(n, num_bombas)
+
+
